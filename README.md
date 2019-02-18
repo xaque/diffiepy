92b5ea8fb6bb18fa419155550be13a11ac24abaa
xaque <zacka999@gmail.com>
2019-02-17 18:09:59 -0700

    Completed Diffie-Hellman sample

---
+++ README.md
@@ -0,0 +1,13 @@
+# Project 4: Diffie-Hellman
+
+Run the passoff code with
+
+`python3 diffie.py`
+
+or
+
+`./diffie.py`
+
+You will need `openssl` installed. It should work on Linux and probably macOS. It will generate the numbers p and a and then print them out along with g<sup>a</sup> mod p. Then it will prompt user input for g<sup>b</sup> mod p, after which it will calculate the secret g<sup>ba</sup> mod p.
+
+![passoff.png](passoff.png)
--- diffie.py
+++ diffie.py
@@ -1,12 +1,88 @@
-import math
+#!/usr/bin/env python3
 
+from random import SystemRandom
+import subprocess
+
+# Modular exponentiation
 def mod_exp(x, y, N):
-    # Base case: x^0 mod N = 0
-    if (y == 0):
-        return 1
-    z = mod_exp(x, math.floor(y/2), N)
-    if (y % 2 == 0):
-        return (z**2) % N
-    else:
-        return (x * (z**2)) % N
+    if N == 1:
+        return 0
+    result = 1
+    x = x % N
+    while y > 0:
+        if (y % 2 == 1):
+            result = (result * x) % N
+        y >>= 1
+        x = (x * x) % N
+    return result
 
+# Generate a cryptographically strong prime p where (p-1)/2 is also prime
+def generate_p():
+    rounds = 0
+    prime_found = False
+    while not prime_found:
+        rounds += 1
+        # Generate 512-bit prime with OpenSSL
+        cmd = 'openssl prime -generate -bits 512'
+        proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
+        prime = proc.stdout.read().decode("utf-8")
+        prime = int(prime[:-1])
+
+        # Calculate p and check for primality with OpenSSL
+        p = int ((prime * 2) + 1 )
+        is_prime = 0
+        cmd = 'openssl prime -checks 20 ' + str(p)
+        proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
+        output = proc.stdout.read().decode("utf-8")
+        output = output[:-1]
+        words = output.split(" ")
+        is_prime = words[-2] != 'not'
+
+        # Return p if it is prime
+        if is_prime:
+            prime_found = True
+            #print(rounds)
+            return p
+
+# Generate random 512-bit number through os.urandom()
+def generate_a():
+    return int(SystemRandom().random() * (2**512))
+
+# For debug mode of the passoff
+def debug():
+    p = generate_p()
+    a = generate_a()
+    g = 5
+    ga = mod_exp(g, a, p)
+    print("p:\t", p)
+    print("g^a:\t", ga)
+
+    b = 1998286638065473057944506344030256054916203227381748916180906390214373930105605405985818224246280726328877245115163209963634633681313092395058312190549
+    gb = mod_exp(g, b, p)
+    print("g^b:\t", gb)
+
+    gab = mod_exp(ga, b, p)
+    gba = mod_exp(gb, a, p)
+    print("g^ab:\t", gab)
+
+    assert(gab == gba)
+
+# Diffie-Hellman passoff routine
+def passoff():
+    # Generate initial numbers
+    a = generate_a()
+    print("a:\t", a)
+    g = 5
+    p = generate_p()
+    print("p:\t", p)
+    # Caluclate g^a mod p
+    ga = mod_exp(g, a, p)
+    print("g^a:\t", ga)
+    # Read g^b mod p
+    gb = input("g^b:\t ")
+    gb = int(gb)
+    # Calculate secret
+    gab = mod_exp(gb, a, p)
+    print("g^ab:\t", gab)
+
+passoff()
